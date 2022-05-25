acd9838faf8b62ca0d7a64838dde32b93142dfc6
CosmicOdeum <59748847+OdeumBiomes@users.noreply.github.com>
2022-05-25 22:31:28 +0200

    allowedbiomes

--- Def Classes/ExpandedSongDef.cs
+++ Def Classes/ExpandedSongDef.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace MusicExpanded.Def_Classes
 {
     public class ExpandedSongDef : SongDef
     {
+        public List<BiomeDef> allowedBiomes; 
     }
 }

9aed9339d27ffdf215ceebe520625a0ea1acdf9d
Daniel Caaz <dcaazy@gmail.com>
2022-06-12 13:39:09 -0700

    don't play the same song twice

--- Source/Utilities.cs
+++ Source/Utilities.cs
@@ -6,10 +6,13 @@ namespace MusicExpanded
 {
     public static class Utilities
     {
+        private static TrackDef lastPlayed;
         public static bool AppropriateNow(TrackDef track)
         {
             // Figure out if a track is appropriate right now.
             // Map map = Find.AnyPlayerHomeMap ?? Find.CurrentMap;
+            if (lastPlayed == track) return false;
+            lastPlayed = track;
             return true;
         }
     }

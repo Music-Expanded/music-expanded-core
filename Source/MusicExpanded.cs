6921099c9bf0b21974a22033d277c59fc6458d97
Daniel Caaz <dcaazy@gmail.com>
2022-06-21 23:02:01 -0700

    Add settings, with translated values, and validate it!

--- Source/MusicExpanded.cs
+++ Source/MusicExpanded.cs
@@ -1,12 +1,25 @@
 using HugsLib;
+using HugsLib.Settings;
+using Verse;
 
 namespace MusicExpanded
 {
     public class Core : ModBase
     {
+        public static SettingHandle<string> selectedTheme;
         public override void DefsLoaded()
         {
-            Logger.Message("Hello, World!");
+            selectedTheme = Settings.GetHandle<string>(
+                "selectedTheme",
+                "ME_Theme".Translate(),
+                "ME_ThemeDescription".Translate(),
+                "ME_Glitterworld",
+                (string value) =>
+                {
+                    // Validates that there is a ThemeDef that exists with this defName
+                    return DefDatabase<ThemeDef>.GetNamedSilentFail(value) != null;
+                }
+            );
         }
     }
 }
